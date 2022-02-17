caa30b8ecbd6f893228f026b81a47c9025af171d
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-02-17 15:34:47 +0200

    Implement the logic

--- app.js
+++ app.js
@@ -1,69 +1,72 @@
-const options = document.querySelectorAll('.options-btn');
-const buttons = document.querySelectorAll('.btn');
-
-options.forEach((option) => option.addEventListener('click', startGame));
-
-let rounds = 0;
-let playerScore = 0;
-let computerScore = 0;
-let round = 0;
+const gameBox = document.querySelector('.game-box');
+const options = document.querySelector('.options');
+const actions = document.querySelectorAll('.btn');
+options
+  .querySelectorAll('.options-btn')
+  .forEach((option) => option.addEventListener('click', startGame));
 
 function startGame(e) {
-  buttons.forEach((button) => button.addEventListener('click', playRound));
-  rounds = e.target.textContent;
+  options.classList.add('fade-out');
+  gameBox.classList.add('fade-in');
 
-  document.querySelector('.buttons').classList.add('fade-in');
-  document.querySelector('.options').classList.add('fade-out');
-  setInterval(() => {
-    document.querySelector('.game-box').classList.add('fade-in');
-  }, 500);
-}
+  const bestOf = e.target.textContent;
+  let playerScore = document.querySelector('.player-score');
+  let computerScore = document.querySelector('.computer-score');
+  let currentRound = document.querySelector('.round');
 
-function playRound(e) {
-  const playerSelection = e.target.textContent.toLowerCase();
-  const computerSelection = computerPlay();
+  actions.forEach((action) => action.addEventListener('click', playRound));
 
-  console.log(`You chose ${playerSelection}.`);
-  console.log(`Computer chose ${computerSelection}.`);
+  function playRound() {
+    actions.forEach((action) => action.removeEventListener('click', playRound));
+    const playerSelection = this.dataset.name;
+    const computerSelection = computerPlay();
+    const result = document.querySelector('.buttons-heading');
 
-  let roundWinner = '';
-  switch (true) {
-    case playerSelection == 'rock' && computerSelection == 'scissors':
-    case playerSelection == 'paper' && computerSelection == 'rock':
-    case playerSelection == 'scissors' && computerSelection == 'paper':
-      roundWinner = 'player';
-      break;
-    case playerSelection == 'rock' && computerSelection == 'paper':
-    case playerSelection == 'paper' && computerSelection == 'scissors':
-    case playerSelection == 'scissors' && computerSelection == 'rock':
-      roundWinner = 'computer';
-      break;
-    case playerSelection == computerSelection:
-      roundWinner = 'tie';
-      break;
+    console.log(`You chose ${playerSelection}.`);
+    console.log(`Computer chose ${computerSelection}.`);
+
+    let roundWinner = '';
+    switch (true) {
+      case playerSelection === 'rock' && computerSelection == 'scissors':
+      case playerSelection === 'paper' && computerSelection == 'rock':
+      case playerSelection === 'scissors' && computerSelection == 'paper':
+        roundWinner = 'player';
+        break;
+      case playerSelection === 'rock' && computerSelection == 'paper':
+      case playerSelection === 'paper' && computerSelection == 'scissors':
+      case playerSelection === 'scissors' && computerSelection == 'rock':
+        roundWinner = 'computer';
+        break;
+      case playerSelection === computerSelection:
+        roundWinner = 'tie';
+        break;
+    }
+
+    if (roundWinner === 'player') {
+      playerScore.textContent = Number(playerScore.textContent) + 1;
+      result.textContent = 'You win!';
+    } else if (roundWinner === 'computer') {
+      computerScore.textContent = Number(computerScore.textContent) + 1;
+      result.textContent = 'You loose!';
+    } else {
+      result.textContent = "It's a tie!";
+    }
+
+    if (playerScore.textContent === bestOf) {
+      result.textContent = 'You won!';
+      return;
+    } else if (computerScore.textContent === bestOf) {
+      result.textContent = 'You lost!';
+      return;
+    }
+
+    currentRound.textContent = Number(currentRound.textContent) + 1;
+    actions.forEach((action) => action.addEventListener('click', playRound));
   }
 
-  renderRoundResult(roundWinner);
-}
-
-function computerPlay() {
-  const signs = ['rock', 'paper', 'scissors'];
-  const randomIndex = Math.floor(Math.random() * 3);
-  return signs[randomIndex];
-}
-
-function renderRoundResult(roundWinner) {
-  if (roundWinner === 'player') {
-    const score = document.querySelector('#playerScore');
-    let result = Number(score.textContent);
-    result += 1;
-    score.textContent = result;
-  } else if (roundWinner === 'computer') {
-    const score = document.querySelector('#computerScore');
-    let result = Number(score.textContent);
-    result += 1;
-    score.textContent = result;
-  } else {
-    console.log('TIE!');
+  function computerPlay() {
+    const signs = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return signs[randomIndex];
   }
 }
